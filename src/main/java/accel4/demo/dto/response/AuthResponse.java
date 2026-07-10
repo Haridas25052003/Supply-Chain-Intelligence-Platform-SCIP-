8dc16a0743a130a026f498c1389c6c4f6888579f
Haridas Shinde <haridasshinde314@gmail.com>
2026-07-10 21:44:47 +0530

    added dto and jwt

---
+++ src/main/java/accel4/demo/dto/response/AuthResponse.java
@@ -0,0 +1,17 @@
+package accel4.demo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String email;
+    private String message;
+}
